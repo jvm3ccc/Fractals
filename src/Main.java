df118f73fd557868d44a626db5218f3307d25897
mszkb <martin.szalay.kb@gmail.com>
2017-08-19 20:28:16 +0200

    Documented

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,9 @@ public class Main {
     }
 
     private static void level2() {
+
+        // Theorie: http://snowflakecurve.weebly.com/square-curve.html
+
         for (int i = 0; i <= iterationen; i++) {
 
             if( i == 0 ) {
@@ -47,14 +50,50 @@ public class Main {
         System.out.println("Ergebnis1: " + result);
     }
 
-    private static int calcN2(int n) {
-        return 4 * (int) Math.pow(5, n);
-    }
 
+
+    /**
+     * Anzahl der Seiten multipliziert mit den Segmenten
+     * Bei Dreieck 3 Seiten
+     * Bei 4-Eck 4 Seiten
+     *
+     * Dreieck 4 Segmente
+     * Viereck 5 Segemente
+     *
+     * @param n - Iteration
+     * @return Anzahl der Seiten multipliziert mit der Laengen
+     */
     private static int calcN(int n) {
         return 3 * (int) Math.pow(4, n);
     }
 
+    /**
+     * Anzahl der Seiten multipliziert mit den Segmenten
+     * Bei Dreieck 3 Seiten
+     * Bei 4-Eck 4 Seiten
+     *
+     * Dreieck 4 Segmente
+     * Viereck 5 Segemente
+     *
+     * @param n - Iteration
+     * @return Anzahl der Seiten multipliziert mit der Laengen
+     */
+    private static int calcN2(int n) {
+        return 4 * (int) Math.pow(5, n);
+    }
+
+    /**
+     * Mit jeder Iteration wird ein Drittel der Laenge berechnet
+     *
+     * zB
+     * 0 .. n
+     * 1 .. n/3
+     * 2 .. n/9
+     * usw
+     *
+     * @param n - Iteration i siehe Schleife
+     * @return Laenge einer Seite
+     */
     private static float calcL(int n) {
         return laenge * (float) Math.pow(3, -n);
     }
