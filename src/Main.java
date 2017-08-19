5f1b17f0bdf459455a72661d13a93836a0c04b0a
mszkb <martin.szalay.kb@gmail.com>
2017-08-19 18:30:32 +0200

    wrong result calculation. Dont sum up, just get the n-th iteration

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,23 @@
 public class Main {
 
-    public static int laenge = 19683;
-    public static int iterationen = 7;
+    public static int laenge = 243;
+    public static int iterationen = 3;
     public static int result = 0;
 
     public static void main(String[] args) {
 
         // Theorie: http://www.fraktalkunst.de/2012/02/03/koch-kurve-und-schneeflocke/
 
-        for( int i = 1; i <= iterationen; i++) {
+        for( int i = 0; i <= iterationen; i++) {
+
+            if( iterationen == 0 ) {
+                result = laenge;
+                continue;
+            }
+
             int L = calcL(i);
             int N = calcN(i);
-            result += L * N;
+            result = L * N;
         }
 
         System.out.println("Ergebnis: " + result);
