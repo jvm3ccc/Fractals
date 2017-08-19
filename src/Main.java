ef722aec32e0a7e4c71f4267fa9d2c96a83b2319
mszkb <martin.szalay.kb@gmail.com>
2017-08-19 20:15:47 +0200

    implement level2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,34 @@
 public class Main {
 
-    private static float laenge = 531441;
+    private static float laenge = 531441f;
     private static int iterationen = 9;
     private static int result = 0;
 
     public static void main(String[] args) {
+
+        level1();
+        level2();
+
+    }
+
+    private static void level2() {
+        for (int i = 0; i <= iterationen; i++) {
+
+            if( i == 0 ) {
+                result = (int) laenge;
+                continue;
+            }
+
+            int L = (int) calcL(i);
+            int N = (int) calcN2(i);
+
+            result = (int) (L * N);
+        }
+
+        System.out.println("Ergebnis2: " + result);
+    }
+
+    private static void level1() {
 
         // Theorie: http://www.fraktalkunst.de/2012/02/03/koch-kurve-und-schneeflocke/
 
@@ -20,8 +44,11 @@ public class Main {
             result = (int) (L * N);
         }
 
-        System.out.println("Ergebnis: " + result);
+        System.out.println("Ergebnis1: " + result);
+    }
 
+    private static int calcN2(int n) {
+        return 4 * (int) Math.pow(5, n);
     }
 
     private static int calcN(int n) {
